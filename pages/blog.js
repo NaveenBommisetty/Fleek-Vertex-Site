0f8d052ace3a8f1ab427b67479ae759421b8b938
omprakash6054 <omprakash6054@gmail.com>
2024-09-22 16:51:44 +0100

    blog

--- pages/blog.js
+++ pages/blog.js
@@ -12,10 +12,10 @@ const Blog = () => {
 
             <Layout>
                 <section className="section banner-service bg-grey-60 position-relative">
-                    <div className="box-banner-abs">
+                    {/* <div className="box-banner-abs">
                         <div className="container">
                             <div className="row align-items-center">
-                                <div className="col-xxl-6 col-xl-7 col-lg-12">
+                                <div className="col-xxl-6 col-xl-6 col-lg-12">
                                     <div className="box-banner-service">
                                         <h2 className="color-brand-5 mb-20">All the important insights, guidance and news you need to know.</h2>
                                         <div className="row">
@@ -27,11 +27,22 @@ const Blog = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                     <div className="row m-0">
-                        <div className="col-xxl-6 col-xl-6 col-lg-6" />
-                        <div className="col-xxl-6 col-xl-6 col-lg-6 pr-0">
-                            <div className="d-none d-xxl-block pl-70"><img className="w-100 d-block" src="assets/imgs/page/blog/banner.png" alt="iori" /></div>
+                        <div className="col-xxl-6 col-xl-12 col-lg-6 col-md-12" >
+                        <div className="pl-100 pt-30 align-items-center">
+                                    <div className="box-banner-service">
+                                        <h2 className="color-brand-5 mb-20">All the insights and news you need to know.</h2>
+                                        <div className="row">
+                                            <div className="col-lg-9">
+                                                <p className="font-lg color-grey-900">Keep up-to-date with all our latest company news and business content. The latest news, tips and advice to help you run your business with less fuss</p>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xxl-6 col-lg-6 col-md-6 pr-0">
+                            <div className="d-none d-lg-block pl-50"><img className="w-100 d-block" src="assets/imgs/page/blog/banner.png" alt="iori" /></div>
                         </div>
                     </div>
                 </section>
