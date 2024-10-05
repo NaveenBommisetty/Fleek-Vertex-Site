0288064ffd1982a1db7f65d2fe49ab5249c0ab8b
omprakash6054 <omprakash6054@gmail.com>
2024-10-05 18:21:42 +0100

    contact form is working

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -8,26 +8,41 @@ export default async function handler(req, res) {
     console.log(req.body);
 
     let transporter = nodemailer.createTransport({
-      host: 'smtp.mailersend.net', // SMTP server
-      port: 587, // TLS uses port 587
-      secure: false, // TLS is not the same as SSL (use false for port 587)
+      host: 'fleekvertex.com', // SMTP server
+      port: 465, // TLS uses port 587
+      secure: true, // TLS is not the same as SSL (use false for port 587)
       auth: {
-        user: 'ms_xfkzhf@fleekvertex.com', // Username
+        user: 'naveen@fleekvertex.com', // Username
         pass: 'KgwUpx8wEBKEk9wf', // Password
       },
       tls: {
+        ciphers: "SSLv3",
         rejectUnauthorized: false, // Allow self-signed certificates if needed
       },
     });
 
     try {
-      await transporter.verify(); // Check if the SMTP server is reachable
+      await transporter.verify((err, success) => {
+        if (err) {
+          console.log(err);
+          
+        }
+      }); // Check if the SMTP server is reachable
 
       await transporter.sendMail({
-        from: `"${name}" <${email}>`,
-        to: 'ms_xfkzhf@fleekvertex.com', // Send to your email
+        // from: `"${name}" <${email}>`,
+        from: 'naveen@fleekvertex.com',
+        to: 'naveen@fleekvertex.com', // Send to your email
         subject: subject,
-        text: message,
+        text: message, 
+          html: `
+          <p>Name: ${name} </p>
+          <p>Email: ${email} </p>
+          <p>Phone: ${phone} </p>
+          <p>Company: ${company} </p>
+          <p>Message: ${message} </p>
+          `,         
+          
       });
 
       res.status(200).json({ message: 'Email sent successfully!' });
